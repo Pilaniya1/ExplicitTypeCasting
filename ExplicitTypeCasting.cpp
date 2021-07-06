395a1810743e7491dd5cea316ecf01c35e283f3b
Pilaniya1 <82079006+Pilaniya1@users.noreply.github.com>
2021-07-06 14:40:26 +0530

    Add files via upload
    The output will be:-
    intvar=25
    floatvar=35.87
    float(intvar)=25
    int(floatvar)=35
    *how explicit type casting.

---
+++ ExplicitTypeCasting.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int intvar=25;
+    float floatvar=35.87;
+    cout<<"intvar"<<intvar<<endl;
+    cout<<"floatvar"<<floatvar<<endl;
+    cout<<"\nfloat(intvar) ="<<float(intvar);
+    cout<<"\nint(floatvar) ="<<int(floatvar);
+    getchar;
+}
